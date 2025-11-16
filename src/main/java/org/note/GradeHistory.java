49a7bd82bd5ebd7e315d637df756f93f7661cb47
Lahatranomena <hei.lahatra.3@gmail.com>
2025-11-16 19:40:07 +0300

    modify class GradeHistory

--- src/main/java/org/note/GradeHistory.java
+++ src/main/java/org/note/GradeHistory.java
@@ -1,4 +1,39 @@
 package org.note;
 
+import java.time.Instant;
+
 public class GradeHistory {
+    private double oldValue;
+    private double newValue;
+    private Instant changeAt;
+    private String reason;
+    private String comment;
+
+    public GradeHistory(double oldValue, double newValue, Instant changeAt, String reason, String comment) {
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+        this.changeAt = changeAt;
+        this.reason = reason;
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Instant getChangeAt() {
+        return changeAt;
+    }
+
+    public double getNewValue() {
+        return newValue;
+    }
+
+    public double getOldValue() {
+        return oldValue;
+    }
 }
